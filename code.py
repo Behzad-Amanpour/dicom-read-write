be7d243037403e2b41e771f8b2888fc4310011f2
Behzad-Amanpour <116773990+Behzad-Amanpour@users.noreply.github.com>
2023-01-24 11:55:16 +0330

    Update code.py

--- code.py
+++ code.py
@@ -1,14 +1,19 @@
+"""
+Inputs:
+    You can use "Image.dcm" which I uploaded as a file
+"""
+
+# Reading a DICOM file ================================== Behzad Amanpour =======================
 from pydicom import dcmread
 import matplotlib.pyplot as plt
-import numpy as np
+import numpy as np  # You may or may not need numpy
 
+f = dcmread('An address on you computer or google drive\\Image.dcm')   # This file contains a "header" and a "image" 
+Im = f.pixel_array   # Exctracting the image from the file
+plt.imshow(Im,cmap='gray')   # Showing the image
 
-f=dcmread('D:\\Work\\Insta\\Amanpour.vip\\Image_Processing\\2_ReadingImage\\Image.dcm')
-
-
-Im=f.pixel_array
-plt.imshow(Im,cmap='gray')
-
+# Reading & Changing Header Info ======================== Behzad Amanpour ========================
+print (f)
 
 f.PatientName ='Patient^One'
 f.PatientID = '111111'
@@ -19,8 +24,8 @@ f.PatientAge = '50'
 f.PatientBirthDate = '1972'
 f.PatientID = '111111'
 f.StationName = 'GEHCGEHC'
-f.StudyDate = '20191113'
+f.StudyDate = '20191113'   # there are much more info in the header
 
+# Writing a new DICOM file ============================== Behzad Amanpour ========================
 
-
-f.save_as("C:\\Users\\amanp\\Desktop\\Image.dcm")
+f.save_as("An address on you computer or google drive\\Image2.dcm")
